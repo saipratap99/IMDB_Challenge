cf165e5fedf51b4c18025d244bc43e35f138e602
saipratap99 <122005067@sastra.ac.in>
2021-09-16 19:41:43 +0530

    Added all the associations for Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
 class Movie < ApplicationRecord
   belongs_to :category
   belongs_to :language
+  has_many :movie_genres, dependent: :destroy
+  has_many :genres, through: :movie_genres
+  has_many :movie_casts, dependent: :destroy
+  has_many :cast_crew_detail, through: :movie_casts
+  has_many :movie_crews, dependent: :destroy
+  has_many :cast_crew_detail, through: :movie_crews
 end
