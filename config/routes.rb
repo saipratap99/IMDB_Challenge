b84ef3ef6f90eedd3b1a9965660a88170e6dff6b
saipratap99 <122005067@sastra.ac.in>
2021-09-16 20:44:07 +0530

    Added header for index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   resources :movie_crews
   resources :movie_casts
   resources :movies
   resources :categories
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
