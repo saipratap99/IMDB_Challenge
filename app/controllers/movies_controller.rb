1bd50dacc0fd2d0b806f6073c9e32c0c2369006f
saipratap99 <122005067@sastra.ac.in>
2021-09-17 09:44:47 +0530

    Show page done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,7 @@ class MoviesController < ApplicationController
 
   # GET /movies/1 or /movies/1.json
   def show
+    @movie = Movie.find(params[:id])
   end
 
   # GET /movies/new
@@ -57,13 +58,14 @@ class MoviesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def movie_params
-      params.require(:movie).permit(:title, :release_date, :rating, :trailer_link, :description, :story_line, :category_id, :language_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def movie_params
+    params.require(:movie).permit(:title, :release_date, :rating, :trailer_link, :description, :story_line, :category_id, :language_id)
+  end
 end
