8e138d2678f79cdae4d084c7df79b4ac60f8bdd1
saipratap99 <122005067@sastra.ac.in>
2021-09-18 19:07:21 +0530

    Embeded video

--- app/models/cast_crew_detail.rb
+++ app/models/cast_crew_detail.rb
@@ -1,2 +1,6 @@
 class CastCrewDetail < ApplicationRecord
+  has_many :movie_casts
+  has_many :movies, through: :movie_casts
+  has_many :movie_crews
+  has_many :movies, through: :movie_crews
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,9 +6,9 @@ class Movie < ApplicationRecord
   has_many :movie_genres, dependent: :destroy
   has_many :genres, through: :movie_genres
   has_many :movie_casts, dependent: :destroy
-  has_many :cast_crew_detail, through: :movie_casts
+  has_many :cast_crew_details, through: :movie_casts
   has_many :movie_crews, dependent: :destroy
-  has_many :cast_crew_detail, through: :movie_crews
+  has_many :cast_crew_details, through: :movie_crews
 
   def self.add_record(movie_hash)
     if (movie_hash["category"] != nil and movie_hash["release_date"] != nil)
@@ -20,6 +20,8 @@ class Movie < ApplicationRecord
                              trailer_link: movie_hash["trailer_link"], description: movie_hash["description"], story_line: movie_hash["story_line"],
                              category_id: category.id, language_id: language.id)
       end
+      MovieDetail.create(runtime: movie_hash["runtime"], movie_id: movie.id)
+
       # adding genres
       genres_str = movie_hash["movie_genres"]
       if (!genres_str.nil? and !genres_str.empty?)
@@ -33,22 +35,31 @@ class Movie < ApplicationRecord
       for i in 1..3
         # adding photos
         url = movie_hash[("photo" + i.to_s)]
-        photo = Photo.create(img_url: url, movie_id: movie.id) if !url.nil? and !url.empty?
+        photo = Photo.create({ img_url: url, movie_id: movie.id }) if !url.nil? and !url.empty?
 
         role, cast_name = movie_hash[("cast" + i.to_s)].split(":").map { |str| str.strip } if !movie_hash["cast" + i.to_s].nil?
         dept_name, crew_name = movie_hash[("crew" + i.to_s)].split(":").map { |str| str.strip } if !movie_hash["crew" + i.to_s].nil?
 
         # adding cast
-        if (!role.nil? and !cast_name.nil? and !role.empty? and !cast_name.empty?)
-          cast = CastCrewDetail.where("lower(name) = ?", cast_name.downcase).first_or_create
-          movie_cast = MovieCast.create(role_name: role, cast_crew_detail_id: cast.id, movie_id: movie.id)
+        if (!role.nil? && !cast_name.nil? && !role.empty? && !cast_name.empty?)
+          cast = CastCrewDetail.where("lower(name) = ?", cast_name.downcase).first
+          if (cast.nil?)
+            cast = CastCrewDetail.create({ name: cast_name })
+          end
+          movie.movie_casts.create({ role_name: role, cast_crew_detail_id: cast.id })
         end
 
         # adding crew
-        if (!dept_name.nil? and !crew_name.nil? and !dept_name.empty? and !crew_name.empty?)
-          dept = Department.where("lower(name) = ?", dept_name.downcase).first_or_create
-          crew = CastCrewDetail.where("lower(name) = ?", crew_name.downcase).first_or_create
-          movie_crew = MovieCrew.create(department_id: dept.id, cast_crew_detail_id: crew.id, movie_id: movie.id)
+        if (!dept_name.nil? && !crew_name.nil? && !dept_name.empty? && !crew_name.empty?)
+          dept = Department.where("lower(name) = ?", dept_name.downcase).first
+          if (dept.nil?)
+            dept = Department.create({ name: dept_name })
+          end
+          crew = CastCrewDetail.where("lower(name) = ?", crew_name.downcase).first
+          if (crew.nil?)
+            crew = CastCrewDetail.create({ name: crew_name })
+          end
+          movie.movie_crews.create({ department_id: dept.id, cast_crew_detail_id: crew.id })
         end
       end
 
